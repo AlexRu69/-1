533094698322618b365b963417f2579045763ac2
Alex Ru <Alex.ru.tver@gmail.com>
2023-02-11 02:09:39 +0300

    23 Задача

---
+++ Задача21/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 21: Напишите программу, которая принимает
+// на вход координаты двух точек и находит расстояние
+// между ними в 3D пространстве.
+
+
+Console.Clear();
+
+Console.Write("Введите координату x0: ");
+double x0 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату y0: ");
+double y0 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату z0: ");
+double z0 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату x1: ");
+double x1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату y1: ");
+double y1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату z1: ");
+double z1 = double.Parse(Console.ReadLine()!);
+
+double A = Math.Pow(x1 - x0, 2) + Math.Pow(y1 - y0, 2) + Math.Pow(z1 - z0, 2);
+
+double result = Math.Sqrt(A);
+
+Console.WriteLine($"{result:f2}");
---
+++ Задача23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №19. Общее обсуждение
+// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу кубов чисел
+// от 1 до N.
+
+
+Console.Clear();
+
+Console.Write("Введите число N: ");
+double N = double.Parse(Console.ReadLine()!);
+double count = 1;
+while (count <= N)
+{
+                                   //2
+    Console.Write($"{Math.Pow(count, 3)}");
+    if (count != N)
+    {
+        Console.Write(", ");
+    }
+    //Console.Write($"{count * count},");
+    count++;
+}
