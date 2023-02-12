24fc86df9f72da31e3ccbf0ae1c9063a77dc6148
Alex Ru <Alex.ru.tver@gmail.com>
2023-02-12 23:24:35 +0300

    задаёт одномерный массив и выводит на экран

---
+++ Задача29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8
+// элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] GetArrya()   // Метод заданния массива (ручной ввод)
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        int A = int.Parse(Console.ReadLine()!);
+        array[i] = A;
+    }
+    return array;
+}
+
+void PrintArray(int[] inarray)  // Метод вывода массива
+{
+    for (int i = 0; i < inarray.Length; i++)
+    {
+        Console.Write($"{inarray[i]} ");
+    }
+}
+
+int[] A = GetArrya();
+Console.Write("[");
+PrintArray(A);
+Console.Write("]");
