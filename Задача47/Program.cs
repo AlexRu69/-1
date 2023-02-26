884440fb82f28ba6431c069c1a89bb068454c00d
Alex Ru <Alex.ru.tver@gmail.com>
2023-02-26 13:19:40 +0300

    Метод для заполнения матрицы случайными вещественными числами

---
+++ Задача47/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+
+
+Console.Clear();
+
+
+
+double[,] DoubleMatrix(int rows, int columns) // Метод для заполнения матрицы случайными вещественными числами
+{
+    double[,] matrix = new double[rows, columns];
+    var rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = Convert.ToDouble(rand.Next(-100, 100) / 10.0);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.WriteLine("Введите кол-во строк: ");
+int line = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+double[,] newArray = (DoubleMatrix(line, col));
+PrintMatrix(newArray);
+System.Console.WriteLine();
