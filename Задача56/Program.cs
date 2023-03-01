7fb2159327ee7683d3a2a95f326aeca32fe92c18
Alex Ru <Alex.ru.tver@gmail.com>
2023-03-02 00:25:28 +0300

    Считает сумму элементов строк, выдаёт индекс строк с мин суммой

--- Задача56/Program.cs
+++ Задача56/Program.cs
@@ -28,21 +28,44 @@ void PrintAraay2D(int[,] array)
     }
 }
 
-void GetSum(int[,] matrix)  
+int[] GetSum(int[,] matrix)   // считает сумму элементов строк [,]массива и записывает их в одномерный массив
 {
+    int[] sumString = new int[matrix.GetLength(0)];
+    int index = 0;
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        double sum = 0;
-        double average = 0;
+        int sum = 0;
+
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             sum = sum + matrix[i, j];
         }
-        average = sum;
-        System.Console.WriteLine($"столбец {i}: {average}");
+        sumString[index++] = sum;
+
     }
+    return sumString;
 }
 
-int[,] newAArray = GetArray2D(3, 3, 1, 3);
-PrintAraay2D(newAArray);
-GetSum(newAArray);
+// int GetResult(int[] array)
+// {
+
+//     int res = array[0];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] < res) res = array[i];
+//     }
+//     return res;
+//     //System.Console.WriteLine($"Строка с наименьшей суммой элементов -> {res}");
+// }
+
+int[,] newAArray2D = GetArray2D(5, 3, 1, 9);
+PrintAraay2D(newAArray2D);
+GetSum(newAArray2D);
+System.Console.WriteLine();
+System.Console.WriteLine(string.Join(", ", GetSum(newAArray2D)));
+System.Console.WriteLine();
+int[] newArray = GetSum(newAArray2D);
+// System.Console.WriteLine(GetResult(newArray));
+
+int idx = Array.IndexOf(newArray, newArray.Min());    //Выдаёт индекс и значение минимальной строки.
+Console.WriteLine($"index of min = {idx}, value of min = {newArray[idx]}");
