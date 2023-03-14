8859050bc1365d30a51024510db1847a69fedde2
Alex Ru <Alex.ru.tver@gmail.com>
2023-03-14 19:51:33 +0300

    Первая прикидка

---
+++ Итоговая/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу которая из имеющегося массива строк создаёт
+//  массив в котором все строки меньше иле ровны
+// трём символам. 
+
+Console.Clear();
+
+string[] str = { "hello", "123", "string", "5", ",.&", "homework" };
+int index = 0;
+
+
+//string[] GetArrayWhoseElementAreLessThan3Char(string[] array)
+
+while (index < str.Length)
+{
+    string a = str[index];
+    if (a.Length <= 3)
+    {
+        str[index] = a;
+        index++;
+        System.Console.WriteLine(str[index]);
+    }
+}
+
+
